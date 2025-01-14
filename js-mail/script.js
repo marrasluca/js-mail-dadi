de61af5de939a36c25294c9c001c6b641648a15d
marrasluca <lmarras10@gmail.com>
2025-01-14 16:15:40 +0100

    add: start

---
+++ js-mail/script.js
@@ -0,0 +1,8 @@
+/*
+Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+*/
